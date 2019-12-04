73592a6b931449cd75c52bc4ec0964660a6abf7a
181537 <tomasz.kosiorski@santander.pl>
2019-12-04 13:31:42 +0100

    working on day3

---
+++ src/main/java/pl/kosiorski/adventofcode2019/day3/Day3.java
@@ -0,0 +1,24 @@
+package pl.kosiorski.adventofcode2019.day3;
+
+import java.io.IOException;
+import java.util.List;
+
+import static utils.DataReader.initializeDataStrings;
+
+public class Day3 {
+    public static void main(String[] args) throws IOException {
+
+        final String filePath = "src/main/java/pl/kosiorski/adventofcode2019/day3/input-day3.txt";
+
+        List<String> input = initializeDataStrings(filePath);
+
+
+        System.out.println(part1(input));
+    }
+
+
+    private static int part1(List<String> input) {
+
+        return 0;
+    }
+}
