acb9860eb0e83cb36df1fd2c1517506ae34d5da2
181537 <tomasz.kosiorski@santander.pl>
2019-12-05 12:17:57 +0100

    working on Day5

---
+++ src/main/java/pl/kosiorski/adventofcode2019/day5/Day5.java
@@ -0,0 +1,25 @@
+package pl.kosiorski.adventofcode2019.day5;
+
+import java.io.IOException;
+import java.util.List;
+
+import static utils.DataReader.initializeDataIntegers;
+
+public class Day5 {
+    public static void main(String[] args) throws IOException {
+
+        final String filePath = "src/main/java/pl/kosiorski/adventofcode2019/day5/input-day5.txt";
+
+        List<Integer> input = initializeDataIntegers(filePath);
+
+
+        System.out.println(part1(input));
+    }
+
+
+    private static int part1(List<Integer> input) {
+
+        return 0;
+    }
+
+}
