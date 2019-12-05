00844c160aa5e58c0056c665ac514f0bba201575
181537 <tomasz.kosiorski@santander.pl>
2019-12-05 10:27:11 +0100

    Finished Day4 part2

--- src/main/java/pl/kosiorski/adventofcode2019/day4/Day4.java
+++ src/main/java/pl/kosiorski/adventofcode2019/day4/Day4.java
@@ -10,6 +10,7 @@ public class Day4 {
 
 
         System.out.println(IntStream.range(beginning, end).filter(Day4::part1).count());
+        System.out.println(IntStream.range(beginning, end).filter(Day4::part2).count());
     }
 
     private static boolean part1(int password) {
@@ -25,5 +26,36 @@ public class Day4 {
             if (numbers[i + 1] == numbers[i]) isEqual = true;
         }
         return isIncreasing && isEqual;
+    }
+
+
+    private static boolean part2(int password) {
+
+        char[] numbers = Integer.toString(password).toCharArray();
+
+        boolean isIncreasing = true;
+        boolean isEqual = false;
+        boolean groupsEquals = false;
+
+        int similar = 0;
+
+        for (int i = 1; i < numbers.length; i++) {
+
+            if (numbers[i - 1] == numbers[i]) {
+                similar++;
+                if (i == 5 && similar == 1) {
+                    groupsEquals = true;
+                }
+            } else {
+                if (similar == 1) {
+                    groupsEquals = true;
+                }
+                similar = 0;
+            }
+
+            if (numbers[i - 1] > numbers[i]) isIncreasing = false;
+            if (numbers[i - 1] == numbers[i]) isEqual = true;
+        }
+        return isIncreasing && isEqual && groupsEquals;
     }
 }
